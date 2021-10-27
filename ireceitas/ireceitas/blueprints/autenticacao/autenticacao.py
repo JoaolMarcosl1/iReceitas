24c455b432efd309dc2bfb86240766c0435bd40a
Eduardo Arruda <eduardoarrudag2@gmail.com>
2021-10-27 21:58:09 +0000

    Fiz a configuração e uso do flask_login nas paginas de register e login

--- ireceitas/ireceitas/blueprints/autenticacao/autenticacao.py
+++ ireceitas/ireceitas/blueprints/autenticacao/autenticacao.py
@@ -1,6 +1,7 @@
 from flask import Blueprint, request, redirect, url_for, flash, render_template
 from ...ext.database import db
-
+from ..usuario.entidades import User
+from flask_login import login_user, login_required
 bp = Blueprint('autenticacao', __name__, url_prefix='/autenticacao', template_folder='templates')
 
 
@@ -14,21 +15,21 @@ def register():
         name = request.form['name']
         email = request.form['email']
         pwd = request.form['password']
-#         sobre = ""
+        sobre = ""
 
 
-#         jatem = User.query.filter_by(email=email).first()
+        jatem = User.query.filter_by(email=email).first()
 
-#         if jatem is not None:
-#             flash('Já existe uma conta com esse e-mail. Insira outro e-mail')
-#             return redirect(url_for('register'))
+        if jatem is not None:
+            flash('Já existe uma conta com esse e-mail. Insira outro e-mail')
+            return redirect(url_for('register'))
 
-#         else:
-#             user = User(name, email, pwd, sobre)
-#             db.session.add(user) #inserir
-#             db.session.commit()  #atualiza
-#             flash('Conta criada com sucesso!')
-#             return redirect(url_for('login'))
+        else:
+            user = User(name, email, pwd, sobre)
+            db.session.add(user) #inserir
+            db.session.commit()  #atualiza
+            flash('Conta criada com sucesso!')
+            return redirect(url_for('login'))
 
     return render_template('register.html')
 
@@ -38,13 +39,13 @@ def login():
         email = request.form['email']
         pwd = request.form['password']
 
-        # user = User.query.filter_by(email=email).first()
+        user = User.query.filter_by(email=email).first()
 
-        # if not user or not user.verify_password(pwd):
-        #     flash("Email ou senha inválidos!")
-        #     return redirect(url_for('login'))
+        if not user or not user.verify_password(pwd):
+            flash("Email ou senha inválidos!")
+            return redirect(url_for('login'))
 
-        # login_user(user)
+        login_user(user)
         flash('Você foi logado com sucesso :)\n')
         return redirect(url_for('root'))
 
--- ireceitas/ireceitas/ext/auth.py
+++ ireceitas/ireceitas/ext/auth.py
@@ -1,9 +1,10 @@
 from flask import Flask
-from flask_login import loginManager
+from flask_login import LoginManager
+
 
 from typing import NoReturn
 
-login_manager = loginManager()
+login_manager = LoginManager()
 
 def init_app(app : Flask) -> NoReturn:
     login_manager.init_app(app)
