c320c9372369fcf68ce23c710126686e898a27ca
Eduardo Arruda <eduardoarrudag2@gmail.com>
2021-10-27 22:39:08 +0000

    Adicionei as paginas do carrosel e fiz as rotas para visualiza-las

--- ireceitas/ireceitas/views.py
+++ ireceitas/ireceitas/views.py
@@ -1,8 +1,8 @@
 from flask import render_template, flash
-
+from flask_login import current_user
 def root():
-    # if not current_user.is_authenticated:
-    #     flash("\nVocê não esta logado.")
-    # else:
-    #     flash(f"\n\nOlá {current_user.name}, seja bem vindo(a).")
+    if not current_user.is_authenticated:
+        flash("\nVocê não esta logado.")
+    else:
+        flash(f"\n\nOlá {current_user.name}, seja bem vindo(a).")
     return render_template('index.html')
