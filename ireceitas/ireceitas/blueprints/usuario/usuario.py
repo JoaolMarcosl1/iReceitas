1658f48d2c64aaa4c96a10f9ecc044786d726afa
Eduardo Arruda <eduardoarrudag2@gmail.com>
2021-10-28 20:03:02 +0000

    configurei as rotas do para a blueprint usuario

--- ireceitas/ireceitas/blueprints/usuario/usuario.py
+++ ireceitas/ireceitas/blueprints/usuario/usuario.py
@@ -1,13 +1,20 @@
-from flask import Blueprint, render_template
-
+from flask import Blueprint, render_template, flash
+from flask_login import current_user
 
 bp = Blueprint('usuario', __name__, url_prefix='/usuario', template_folder='templates')
 
 
-@bp.route('/')
-def root():
-    return render_template('edit.html')
+# @bp.route('/')
+# def root():
+#     return render_template('edit.html')
 
+@bp.route('/perfil')
+def perfil():
+    if not current_user.is_authenticated:
+        flash("\nSomente quem esta logado pode acessar o seu perfil.")
+    else:
+        flash(".")
+    return render_template("perfil_user.html")
 
 def init_app(app):
     app.register_blueprint(bp)
