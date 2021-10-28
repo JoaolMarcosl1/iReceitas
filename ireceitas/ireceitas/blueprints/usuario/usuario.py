fbdea02dfea92bd590dba188f1823c86ed5201e9
Eduardo Arruda <eduardoarrudag2@gmail.com>
2021-10-28 20:42:23 +0000

    Tentativa de consertar a parte de editar perfil do  usuario

--- ireceitas/ireceitas/blueprints/usuario/usuario.py
+++ ireceitas/ireceitas/blueprints/usuario/usuario.py
@@ -1,5 +1,8 @@
-from flask import Blueprint, render_template, flash
+from flask import Blueprint, render_template, flash, request, url_for, redirect
 from flask_login import current_user
+from ...ext.database import db
+from .entidades import User
+from sqlalchemy.exc import IntegrityError
 
 bp = Blueprint('usuario', __name__, url_prefix='/usuario', template_folder='templates')
 
@@ -15,6 +18,35 @@ def perfil():
     else:
         flash(".")
     return render_template("perfil_user.html")
+
+@bp.route("/edit/<int:id>", methods=['GET', 'POST'])
+def edit(id):
+    user = User.query.get(id)
+    if request.method == 'POST':
+
+        user.name = request.form['name']
+        user.email = request.form['email']
+        user.sobre = request.form['sobre']
+
+        try:
+            db.session.commit()
+            flash("Alteração feita com sucesso!")
+            return redirect(url_for('root'))
+
+        except IntegrityError:
+            flash("Opa! Esse e-mail ja esta sendo utilizado.")
+            return redirect(url_for('root'))
+
+            #return "E-mail existe"
+            #db.session.commit()
+           # return "Deu certo, você mudou o e-mail"
+       # db.session.commit()
+       # return redirect(url_for('home'))
+        #jatem = User.query.filter_by(email=user.email).first()
+       # if jatem is not None:
+           # return "E-mail existe"
+      #  else:
+    return render_template("edit.html", user=user)
 
 def init_app(app):
     app.register_blueprint(bp)
