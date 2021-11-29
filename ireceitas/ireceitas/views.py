8459e966e472d14bbbf63f98d13c6958d3b2c08d
FranciscoLucasPratProf <FranciscoLucas1@gmail.com>
2021-11-29 14:34:03 +0000

    Remoção do flash da página index

--- ireceitas/ireceitas/views.py
+++ ireceitas/ireceitas/views.py
@@ -1,8 +1,6 @@
-from flask import render_template, flash
-from flask_login import current_user
+from flask import render_template
+
 
 def root():
-    if not current_user.is_authenticated:
-        flash("\nVocê não esta logado.")
     return render_template('index.html')
 
