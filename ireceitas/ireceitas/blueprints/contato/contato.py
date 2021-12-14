57d8f0c1c72ef7a1ae673829fcf11f6a05a1b638
FranciscoLucasPratProf <FranciscoLucas1@gmail.com>
2021-12-14 23:15:07 +0000

    Report usuário

--- ireceitas/ireceitas/blueprints/contato/contato.py
+++ ireceitas/ireceitas/blueprints/contato/contato.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, request, flash,  redirect, url_for
+from flask import Blueprint, render_template, request, flash,  redirect, url_for, abort
 from flask_mail import Message
 from flask_login import current_user
 from ireceitas.ext.mail import mail
@@ -9,6 +9,25 @@ bp = Blueprint('contato', __name__, url_prefix='/contato', template_folder='temp
 # @bp.route('/')
 # def root():
 #     return render_template('suporte_email.html')
+
+
+@bp.route('/report', methods=['GET', 'POST'])
+def report():
+    if not current_user.is_authenticated:
+        if request.method == 'POST':
+            nome = request.form.get('nome')
+            email = request.form.get('email')
+            motivo = request.form.get('motivo')
+            msg = Message(subject=f"Report do usuário - [{nome}]", sender="joaobastos716@gmail.com", recipients=["receitasprojetoint@gmail.com"])
+            msg.body = f"{motivo}"
+            msg.html = render_template('report_email.html', motivo=motivo, nome=nome, email=email)
+            mail.send(msg)
+            flash("Seu report foi enviado com sucesso! Aguarde a staff para tomar as devidas providências")
+            return redirect(url_for('root'))
+    else:
+        return abort(404)
+    return render_template("report.html")
+
 
 @bp.route('/contato', methods=['GET', 'POST'])
 def contato():
